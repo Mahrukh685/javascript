93c3cd14680faac5e3fc22ca37fd34322825994c
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-02-21 02:33:49 +0500

    javascript

---
+++ array.js
@@ -0,0 +1,28 @@
+let array=[58,3,5,75,9,70,43,23,68,56]
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i])
+// }
+
+// array.forEach(printarray);
+// function printarray(item){
+//     console.log(item)
+// }
+// for(let i in array){
+//     console.log(array[i])
+// }
+
+// for(let x of array){
+//     console.log(x," ",array[x])
+// }
+
+// const map1 = array.map(printarray);
+// function printarray(x){
+//     console.log(x)
+
+// }
+
+// i=0;
+// while(i<array.length){
+//     console.log(array[i])
+// i++
+// }
